f6379abe74e14685f8197681a09ca173d9a9d722
Kaan Özcan <knzcnn@gmail.com>
2020-11-17 08:04:36 +0300

    handle pm2 option

--- generators/app/index.js
+++ generators/app/index.js
@@ -18,11 +18,24 @@ module.exports = class extends Generator {
       name    : "appName",
       message : "Name of your project",
       default: this.config.get("appName")
+    }, {
+      type: "confirm",
+      name: "isPM2Enabled",
+      message: "Would you like to have PM2 enabled? (Not recommended for containerized infastructures such as kubernates)",
+      default: this.config.get("isPM2Enabled")
     }]);
   }
 
   createPackageJson () {
     const pkgJson = { dependencies, devDependencies };
+
+    const {
+      isPM2Enabled
+    } = this.answers;
+
+    if (!isPM2Enabled) {
+      delete pkgJson.dependencies.pm2;
+    }
 
     this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
   }
@@ -30,19 +43,34 @@ module.exports = class extends Generator {
   generateInitialStructure () {
     const {
       appName,
+      isPM2Enabled
     } = this.answers;
 
     this.config.set("appName", appName);
+    this.config.set("isPM2Enabled", isPM2Enabled);
 
     const templatePath = this.templatePath("initial-project-structure/**/(*|.*)");
-
+    
     this.fs.copyTpl(
       this.templatePath(templatePath),
       this.destinationRoot(),
       {
-        appName
+        appName,
+        isPM2Enabled
       }
     );
+
+    if (isPM2Enabled) {
+      const pm2TemplatePath = this.templatePath("pm2/**/(*|.*)");
+
+      this.fs.copyTpl(
+        this.templatePath(pm2TemplatePath),
+        this.destinationRoot(),
+        {
+          appName
+        }
+      );
+    }
 
     this.createPackageJson();
     this.config.save();
